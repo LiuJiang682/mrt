37522d7d66b6f8ce9987bbda9d645baa12bbcb2e
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-07-13 14:53:57 +1000

    Refactor the data integrity check to independent step.

--- src/main/java/au/gov/vic/ecodev/mrt/template/processor/file/validator/sl4/Sl4DefaultValidator.java
+++ src/main/java/au/gov/vic/ecodev/mrt/template/processor/file/validator/sl4/Sl4DefaultValidator.java
@@ -23,29 +23,30 @@ public class Sl4DefaultValidator implements Validator {
 	public Optional<List<String>> validate(Map<String, List<String>> templateParamMap,
 			Template dataBean) {
 		List<String> messages = new ArrayList<>();
-		if (ArrayUtils.isNotEmpty(strs)) {
-			if ((Numeral.ONE == strs.length) 
-					&&(Strings.EOF.equalsIgnoreCase(strs[Numeral.ZERO]))) {
-				if (null == templateParamMap) {
-					String Message = "Parameter templateParamMap cannot be null!";
-					messages.add(Message);
-				} else {
-					List<String> expectedRecordsList = templateParamMap.get(Strings.NUMBER_OF_DATA_RECORDS_TITLE);
-					List<String> actualRecordsList = templateParamMap.get(Strings.NUMBER_OF_DATA_RECORDS_ADDED);
-					new DataRecordNumberValidator(expectedRecordsList, 
-							actualRecordsList).validate(messages);
-					
-					new MandatoryRowChecker(templateParamMap.get(Strings.KEY_H0532), 
-							Strings.KEY_H0532).validate(messages);
-
-					new MandatoryRowChecker(templateParamMap.get(Strings.KEY_H0533), 
-							Strings.KEY_H0533).validate(messages);
-				}
-			}
-		}
-		
-		boolean hasErrorMessage = new ErrorMessageChecker(messages).isContainsErrorMessages();
-		return new ValidatorHelper(messages, hasErrorMessage)
+//		if (ArrayUtils.isNotEmpty(strs)) {
+//			if ((Numeral.ONE == strs.length) 
+//					&&(Strings.EOF.equalsIgnoreCase(strs[Numeral.ZERO]))) {
+//				if (null == templateParamMap) {
+//					String Message = "Parameter templateParamMap cannot be null!";
+//					messages.add(Message);
+//				} else {
+//					List<String> expectedRecordsList = templateParamMap.get(Strings.NUMBER_OF_DATA_RECORDS_TITLE);
+//					List<String> actualRecordsList = templateParamMap.get(Strings.NUMBER_OF_DATA_RECORDS_ADDED);
+//					new DataRecordNumberValidator(expectedRecordsList, 
+//							actualRecordsList).validate(messages);
+//					
+//					new MandatoryRowChecker(templateParamMap.get(Strings.KEY_H0532), 
+//							Strings.KEY_H0532).validate(messages);
+//
+//					new MandatoryRowChecker(templateParamMap.get(Strings.KEY_H0533), 
+//							Strings.KEY_H0533).validate(messages);
+//				}
+//			}
+//		}
+//		
+//		boolean hasErrorMessage = new ErrorMessageChecker(messages).isContainsErrorMessages();
+//		return new ValidatorHelper(messages, hasErrorMessage)
+		return new ValidatorHelper(messages, false)
 				.updateDataBeanOrCreateErrorOptional(strs, dataBean);
 	}
 
