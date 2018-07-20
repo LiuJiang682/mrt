433c08bf31525360da33720aad50fc24db19ce2f
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-07-20 13:06:29 +1000

    Added missing methods for list operation.

--- src/main/java/au/gov/vic/ecodev/mrt/template/processor/model/MrtTemplateValue.java
+++ src/main/java/au/gov/vic/ecodev/mrt/template/processor/model/MrtTemplateValue.java
@@ -3,6 +3,10 @@ package au.gov.vic.ecodev.mrt.template.processor.model;
 import java.io.Serializable;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class MrtTemplateValue implements TemplateValue, Serializable {
 
 	/**
@@ -24,5 +28,20 @@ public class MrtTemplateValue implements TemplateValue, Serializable {
 
 	public int getIssueColumnIndex() {
 		return issueColumnIndex;
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
 	}
 }
