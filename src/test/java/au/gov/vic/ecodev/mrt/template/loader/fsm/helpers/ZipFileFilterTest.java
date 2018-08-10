bce52b6e30923f6179d6432b64313c0582129e62
vicey3n <vicey3n@192.168.56.1>
2018-08-10 15:10:17 +1000

    Added handle hypen in file name.

--- src/test/java/au/gov/vic/ecodev/mrt/template/loader/fsm/helpers/ZipFileFilterTest.java
+++ src/test/java/au/gov/vic/ecodev/mrt/template/loader/fsm/helpers/ZipFileFilterTest.java
@@ -18,16 +18,20 @@ public class ZipFileFilterTest {
 	public void shouldFiterOutNotZipFiles() {
 		//Given
 		List<String> fileNames = Arrays.asList("mrt_eco.zip", "mart_eco.txt", "My.java", 
-				"mrt_abc_efc.zip", "mrt_abc_efg_hil.zip");
+				"mrt_abc_efc.zip", "mrt_abc_efg_hil.zip", "mrt_abc-efg.zip", 
+				"mrt_abc-efg_h.zip", "mrt_abc-efg_h_E12.zip");
 		givenTestInstance();
 		//When
 		List<String> zipFiles = testInstance.filterZipFile(fileNames);
 		//Then
 		assertThat(zipFiles, is(notNullValue()));
-		assertThat(zipFiles.size(), is(equalTo(3)));
+		assertThat(zipFiles.size(), is(equalTo(6)));
 		assertThat(zipFiles.get(0), is(equalTo("mrt_eco.zip")));
 		assertThat(zipFiles.get(1), is(equalTo("mrt_abc_efc.zip")));
 		assertThat(zipFiles.get(2), is(equalTo("mrt_abc_efg_hil.zip")));
+		assertThat(zipFiles.get(3), is(equalTo("mrt_abc-efg.zip")));
+		assertThat(zipFiles.get(4), is(equalTo("mrt_abc-efg_h.zip")));
+		assertThat(zipFiles.get(5), is(equalTo("mrt_abc-efg_h_E12.zip")));
 	}
 	
 	private void givenTestInstance() {
